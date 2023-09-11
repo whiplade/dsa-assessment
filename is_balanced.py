482ea6797ef0218046cb14bc5a5cf50bf798384f
whiplade <nmawira13@gmail.com>
2023-09-11 14:07:24 +0300

    is_balanced.py

--- is_balanced.py
+++ is_balanced.py
@@ -1,2 +1,41 @@
-def is_balanced():
-    pass
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        return None
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        return None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+
+def is_balanced(expression):
+    stack = Stack()
+    opening_brackets = "([{"
+    closing_brackets = ")]}"
+    bracket_pairs = {')': '(', '}': '{', ']': '['}
+
+    for char in expression:
+        if char in opening_brackets:
+            stack.push(char)
+        elif char in closing_brackets:
+            if stack.is_empty() or stack.pop() != bracket_pairs[char]:
+                return False
+
+    return stack.is_empty()
+
+
+expression1 = "([]{})"
+expression2 = "([)]"
+print(is_balanced(expression1))  
+print(is_balanced(expression2)) 
