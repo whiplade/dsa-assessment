0bacd83f2dcf11d7cbfdc7cafbc7344aa88bdd24
whiplade <nmawira13@gmail.com>
2023-09-11 14:02:03 +0300

    remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,2 +1,12 @@
-def remove_duplicates():
-    pass
+def remove_duplicates(sequence):
+    identified = set()
+    result = []
+    for i in sequence:
+        if i not in identified:
+            result.append(i)
+            identified.add(i)
+    return result
+
+input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
+result = remove_duplicates(input_sequence)
+print(result) 
