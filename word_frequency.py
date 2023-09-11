9f05bed290d139562718aacb4fccc39902d2d6aa
whiplade <nmawira13@gmail.com>
2023-09-11 14:04:31 +0300

    word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -1,2 +1,12 @@
-def word_frequency():
-    pass
+import string
+
+def word_frequency(sentence):
+    words = sentence.lower().translate(str.maketrans("", "", string.punctuation)).split()
+    frequency = {}
+    for word in words:
+        frequency[word] = frequency.get(word, 0) + 1
+    return frequency
+
+sentence = "This is a test sentence. This sentence is a test."
+result = word_frequency(sentence)
+print(result)
